fea8d96735bf9b27af980dc0e47c2b16ffd4b04b
rkushida <137481609+rkushida@users.noreply.github.com>
2024-03-22 15:45:08 +0900

    wip

--- 6/6.45.c
+++ 6/6.45.c
@@ -1,7 +1,10 @@
-#include <time.h>
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define N 1024
-#define block 1024
+#define n 128
 
 void transpose(int *dst, int *src, int dim) {
     int i, j;
@@ -13,30 +16,68 @@ void transpose(int *dst, int *src, int dim) {
 }
 
 void effective_transpose(int *dst, int *src, int dim) {
-    for (int i = 0; i < dim; i += block) {
-        for (int j = 0; j < dim; j += block) {
-            int a = i*dim + j;
-            int b = j*dim + i;
-            for (int k = 0; k < block; k++) {
-                for (int l = 0; l < block; l++) {
-                    dst[b + l*dim + k] = src[a + k*dim + l];
+    for (int i = 0; i + n <= N; i += n) {
+        for (int j = 0; j + n <= N; j += n) {
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < n; l++) {
+                    dst[(j+l)*dim + i+k] = src[(i+k)*dim + j+l];
                 }
             }
         }
     }
+    int c = (N / n) * n;
+    for (int i = 0; i < c; i++) {
+        for (int j = c; j < N; j++) {
+            dst[j*dim + i] = src[i*dim + j];
+        }
+    }
+    for (int i = c; i < N; i++) {
+        for (int j = 0; j < c; j++) {
+            dst[j*dim + i] = src[i*dim + j];
+        }
+    }
+    for (int i = c; i < N; i++) {
+        for (int j = c; j < N; j++) {
+            dst[j*dim + i] = src[i*dim + j];
+        }
+    }
+}
+
+void set(int *array, int length) {
+    long mod = (long)1 << 32;
+    for (int i = 0; i < length; i++) {
+        *array = rand() % mod;
+        array++;
+    }
+}
+
+void test() {
+    int size = N * N;
+    int src[size], dst[size], dst0[size];
+    set(src, size);
+    transpose(dst, src, N);
+    effective_transpose(dst0, src, N);
+    for (int i = 0; i < size; i++) {
+        assert(dst[i] == dst0[i]);
+    }
+}
+
+void measure() {
+    int size = N * N;
+    int src[size], dst[size];
+    clock_t start, end, duration = 0;
+    for (int i = 0; i < 1000; i++) {
+        set(src, size);
+        start = clock();
+        effective_transpose(dst, src, N);
+        end = clock();
+        duration += end - start;
+    }
+    double duration_in_sec = (double)duration / CLOCKS_PER_SEC;
+    printf("N: %d, n: %d => %lf\n", N, n, duration_in_sec);
 }
 
 int main() {
-    int count = 1024;
-    int size = N * N;
-    int src[size], dst[size];
-    for (int i = 0; i < size; i++) {
-        src[i] = 0;
-    }
-    clock_t start = clock();
-    for (int i = 0; i < count; i++) {
-        effective_transpose(dst, src, N);
-    }
-    clock_t end = clock();
-    printf("%d %lf\n", block, (double)(end - start)/CLOCKS_PER_SEC);
+    test();
+    measure();
 }
---
+++ 6/6.46.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define N 1024
+#define n 128
+
+void col_convert(int *G, int dim) {
+    int i, j;
+
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            G[j*dim + i] = G[j*dim + i] || G[i*dim + j];
+        }
+    }
+}
+
+void effective_col_convert(int *G, int dim) {
+    for (int i = 0; i + n <= dim; i += n) {
+        for (int j = 0; j + n <= dim; j += n) {
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < n; l++) {
+                    G[(j+l)*dim + (i+k)] = G[(j+l)*dim + (i+k)] || G[(i+k)*dim + (j+l)];
+                }
+            }
+        }
+    }
+    int c = (dim / n) * n;
+    for (int i = 0; i < c; i++) {
+        for (int j = c; j < dim; j++) {
+            G[j*dim + i] = G[j*dim + i] || G[i*dim + j];
+        }
+    }
+    for (int i = c; i < dim; i++) {
+        for (int j = 0; j < c; j++) {
+            G[j*dim + i] = G[j*dim + i] || G[i*dim + j];
+        }
+    }
+    for (int i = c; i < dim; i++) {
+        for (int j = c; j < dim; j++) {
+            G[j*dim + i] = G[j*dim + i] || G[i*dim + j];
+        }
+    }
+}
+
+void set(int *G, int dim) {
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) {
+            G[i*dim + j] = (i == j) ? 1 : rand() % 2;
+        }
+    }
+}
+
+void test() {
+    int size = N * N;
+    int G[size];
+    int H[size];
+    int I[size];
+    set(G, N);
+    memcpy(H, G, sizeof(int) * N * N);
+    memcpy(I, G, sizeof(int) * N * N);
+    col_convert(H, N);
+    effective_col_convert(I, N);
+    for (int i = 0; i < size; i++) {
+        assert(H[i] == I[i]);
+    }
+}
+
+void measure() {
+    int size = N * N;
+    int G[size];
+    clock_t start, end, duration = 0;
+    for (int i = 0; i < 1000; i++) {
+        set(G, N);
+        start = clock();
+        effective_col_convert(G, N);
+        end = clock();
+        duration += end - start;
+    }
+    double duration_in_sec = (double)duration / CLOCKS_PER_SEC;
+    printf("N: %d, n: %d => %lf\n", N, n, duration_in_sec);
+}
+
+int main() {
+    test();
+    measure();
+}
